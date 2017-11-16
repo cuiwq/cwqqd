c2227daacb553dd978138a71ca71ef7183a8f9c0
cuiweiqiang <1074366253@qq.com>
2017-11-16 21:57:21 +0800

    hahaha

---
+++ web/js/index.js
@@ -0,0 +1,13 @@
+/*
+* @Author: dhl
+* @Date:   2017-10-24 14:39:44
+* @Last Modified by:   dhl
+* @Last Modified time: 2017-10-24 15:56:02
+*/
+
+$.stellar({
+	// 设置滚动的方向  horizontalScrolling水平方向
+    horizontalScrolling: false,
+    // 是否重新加载页面
+    responsive: true
+});
